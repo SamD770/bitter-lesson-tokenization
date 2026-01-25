1a20a674f07617831e89e8f94fa0fdc0696622cc
SamD770 <samdauncey.sd@gmail.com>
2026-01-25 23:58:53 +0100

    configurable consistency losses (over manual code edit)

--- model/model.py
+++ model/model.py
@@ -669,7 +669,8 @@ def off_policy_flexible_training_step(
         use_off_policy=False, 
         early_output_loss_weight=0.,
         early_exit_advantage_estimate=False, 
-        do_backward_pass=True
+        do_backward_pass=True,
+        rate_consistency_loss="ours"
     ):
     """
     Performs a single training step for the model. 
@@ -747,10 +748,15 @@ def off_policy_flexible_training_step(
         gating_loss = relative_gating_loss_weight * gating_loss
 
         # Hacky additional consistency loss : decrease the mean logits if the true downsample rate exceeds the target.
-        factor = (mean_downsample_prob - downsample_rate_target).detach()
-        mean_logits = on_policy_logits.mean()
-        down_gate_rate_loss = hnet_consistency_loss(on_policy_probs, down_gate_samples, downsample_rate_target)
-        # down_gate_rate_loss = consistency_loss_weight * mean_logits * factor
+
+        if rate_consistency_loss == "hnet":
+            print(f"using hnet consistency loss with weight {consistency_loss_weight}")
+            down_gate_rate_loss = consistency_loss_weight * hnet_consistency_loss(on_policy_probs, down_gate_samples, downsample_rate_target)
+        else:
+            print(f"using our consistency loss with weight {consistency_loss_weight}")
+            factor = (mean_downsample_prob - downsample_rate_target).detach()
+            mean_logits = on_policy_logits.mean()
+            down_gate_rate_loss = consistency_loss_weight * mean_logits * factor
 
         total_loss = ar_loss + gating_loss + down_gate_rate_loss
     else:
