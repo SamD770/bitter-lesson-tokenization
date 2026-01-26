f1f3d0a0ba884fd8cc974e904dcca0694db54477
Sam Dauncey <samdauncey.sd@gmail.com>
2026-01-26 00:01:25 +0000

    enable decoupling of architecture params and optimization params for the architecture

--- training_random_base_model/launch_distributed_local.sh
+++ training_random_base_model/launch_distributed_local.sh
@@ -3,11 +3,13 @@
 # for rtx_3090: model size 18M, batch size 32
 # for A100: model size 130M, batch size 32
 
+
 MODEL_SIZE=$1
 BATCH_SIZE=$2
-UPDOWN_SAMPLER=${3:-"random"}
-DATASET=${4:-"fineweb"}
-SEED=${5:-42}
+RUN_TYPE=${3:-"random"}
+ARCHITECTURE=${4:-"random"}
+DATASET=${5:-"fineweb"}
+SEED=${6:-42}
 
 
 echo "Starting training random base model with seed $SEED at $(date)"
@@ -19,11 +21,11 @@ accelerate launch \
     -m training_random_base_model.run \
     --size $MODEL_SIZE \
     --batch_size $BATCH_SIZE \
-    --run_type $UPDOWN_SAMPLER \
-    --architecture $UPDOWN_SAMPLER \
+    --run_type $RUN_TYPE \
+    --architecture $ARCHITECTURE \
     --dataset $DATASET \
     --seed $SEED \
-
+    2>&1 | tee ${OUTPUT_FILE}
 
 echo "Finished training random base model with seed $SEED at $(date)"
 
